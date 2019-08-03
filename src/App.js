a9412b441f8c53dc26922b801148b0926cad954e
Eric <user@Users-MBP.attlocal.net>
2019-08-03 18:25:31 -0500

    styled the header component and added styles, shop and contact links added as well.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css"
 
 import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from '../src/components/header/header.component'
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route path="/" exact component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
