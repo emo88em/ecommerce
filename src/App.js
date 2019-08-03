153d9555ae8f8017146fd26bddf8559ed6efaae0
Eric <user@Users-MBP.attlocal.net>
2019-08-03 07:31:26 -0500

    added navigation items and withreact hoc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/hompage.component';
+
+const HatsPage = () => {
+	return (
+		<div>
+			<h1>Hats Page!</h1>
+		</div> 
+	)
+}
 
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Switch>
+				<Route path="/" exact component={HomePage} />
+				<Route path="/hats" exact component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-import './menu-item.styles.scss'
+import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({ title, imageUrl, size }) => (
 	<div className={`${size} menu-item`}>
-		<div className="background-image" style={{
-			backgroundImage: `url(${imageUrl})`
-		}} /> 
+		<div
+			className="background-image"
+			style={{
+				backgroundImage: `url(${imageUrl})`
+			}}
+		/>
 		<div className="content">
 			<h1 className="title">{title.toUpperCase()}</h1>
 			<span className="subtitle">SHOP NOW</span>
@@ -14,4 +18,4 @@ const MenuItem = ({title, imageUrl, size}) => (
 	</div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
